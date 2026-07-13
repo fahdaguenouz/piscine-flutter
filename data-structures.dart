25128da6a9a5647be71b5ef470665f985e948e52
fahdaguenouz <faguenouz@gmail.com>
2026-07-13 12:55:23 +0100

    fixing listlisting

--- data-structures.dart
+++ data-structures.dart
@@ -6,7 +6,11 @@ List<Object> listObj = [
   {"name": "Uranus", "age": 2026},
 ];
 List<String> listStr = ['Germany', 'Kazakhstan', 'France', 'England'];
-List<List<Object>> listList= [[listNum],[listObj],[listStr]];
+List<List<Object>> listList= [
+  listNum.cast<Object>(),
+  listObj,
+  listStr.cast<Object>(),
+];
 
 Set<String> setStr = {'Germany', 'Kazakhstan', 'France', 'England'};
 Map<String, int> mapStr = {'Fahd': 23, '3agon': 22, 'Moon': 173};
