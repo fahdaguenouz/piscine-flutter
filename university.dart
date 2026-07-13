eab3632dc3d5fadcca41fbc7f579410c4c3caf39
fahdaguenouz <faguenouz@gmail.com>
2026-07-13 14:47:06 +0100

    university

--- university.dart
+++ university.dart
@@ -2,12 +2,7 @@ class University{
   String _name;
   String _city;
   int? _ranking;
-  University(
-    required String name,
-    required String city,
-    int? ranking
-  ) : _name = name, _city = city, _ranking = ranking;
-
+   University(this._name, this._city, [this._ranking]);
   String get name => _name; 
   String get city => _city;
   int? get ranking => _ranking;
